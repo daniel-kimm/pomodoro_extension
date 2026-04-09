36de1140e052944312827ad83ff9c453acada9b6
Isaiah H <isaiahhashimoto@gmail.com>
2026-04-09 18:08:42 -0500

    Initialize streak watcher on popup load

--- src/popup/main.tsx
+++ src/popup/main.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './popup.css';
+import { startStreakWatcher } from './streakWatcher';
+
+startStreakWatcher();
 
 const rootElement = document.getElementById('root');
 
